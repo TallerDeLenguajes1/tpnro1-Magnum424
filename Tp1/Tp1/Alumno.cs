fca9157a04fbacebbae46a188b83a280fe657d81
Alejandro Arcucci <ale_arc2015@hotmail.com>
2019-09-18 10:39:39 -0300

    Done

--- Tp1/Tp1/Alumno.cs
+++ Tp1/Tp1/Alumno.cs
@@ -5,13 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Tp1
-{
+{   //Creo la clase alumno
     public class Alumno
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Curso { get; set; }
+        //Genero un constructor para los objetos
+        public Alumno(int id, string nombre, string apellido, string curso)
+        {
+            this.Id = id;
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Curso = curso;
+        }
+        //Realizo un override del metodo ToString para poder mostrarlo en la lbx
         public override string ToString()
         {
             return Id + ", " + Nombre + ", " + Apellido + ", " + Curso;
--- Tp1/Tp1/MainWindow.xaml.cs
+++ Tp1/Tp1/MainWindow.xaml.cs
@@ -22,26 +22,64 @@ namespace Tp1
     /// </summary>
     public partial class MainWindow : Window
     {
-        //Creo una lista de alumnos global
+        //Creo una lista de alumnos global y tres writers para cargar los archivos
+        ///Utilizo una bindinglist por recomendacion 
         BindingList<Alumno> alumnos;
+        StreamWriter sw;
+        StreamWriter sw1;
+        StreamWriter sw2;
 
         public MainWindow()
         {
             InitializeComponent();
             //Inicializo la lista
             alumnos = new BindingList<Alumno>();
+            //sw = new StreamWriter(File.Open("Futbol.csv",FileMode.OpenOrCreate));
+            //sw1 = new StreamWriter(File.Open("Voley.csv", FileMode.OpenOrCreate));
+            //sw2 = new StreamWriter(File.Open("Atletismo.csv", FileMode.OpenOrCreate));
+            //Vinculo los elementos de la lbx a los elementos de mi lista alumnos, que se crean y modifican en la lista y se muestran en la lbx
             lbxListin.ItemsSource = alumnos;
             
         }
         //Creo un evento del boton para cargar los alumnos
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Alumno alum = new Alumno();
-            alum.Id = Convert.ToInt32(txbID.Text);
-            alum.Nombre = txbNombre.Text;
-            alum.Apellido = txbApellido.Text;
-            alum.Curso = cbbCursos.Text;
+            //Creo una instancia de un alumno con los datos ingresados en las txb
+            Alumno alum = new Alumno(Convert.ToInt32(txbID.Text), txbNombre.Text, txbApellido.Text, cbbCursos.Text);
+            //Agrego el alumno creado en la lista
             alumnos.Add(alum);
+        }
+
+        private void BtnExport_Click(object sender, RoutedEventArgs e)
+        {
+            sw = new StreamWriter(File.Open("Futbol.csv", FileMode.OpenOrCreate));
+            sw1 = new StreamWriter(File.Open("Voley.csv", FileMode.OpenOrCreate));
+            sw2 = new StreamWriter(File.Open("Atletismo.csv", FileMode.OpenOrCreate));
+            foreach (Alumno alumno in alumnos)
+            {
+                if (alumno.Curso == "Futbol")
+                {
+                    sw.Write($"{alumno.Id};");
+                    sw.Write($"{alumno.Nombre};");
+                    sw.Write($"{alumno.Apellido};\n");
+                    sw.Close();
+                }
+                if(alumno.Curso == "Voley")
+                {
+                    sw1.Write($"{alumno.Id};");
+                    sw1.Write($"{alumno.Nombre};");
+                    sw1.Write($"{alumno.Apellido};\n");
+                    sw1.Close();
+                }
+                if(alumno.Curso == "Atletismo")
+                {
+                    sw2.Write($"{alumno.Id};");
+                    sw2.Write($"{alumno.Nombre};");
+                    sw2.Write($"{alumno.Apellido};\n");
+                    sw2.Close();
+                }
+            }
+            MessageBox.Show("Archivos cargados");
         }
     }
 }
